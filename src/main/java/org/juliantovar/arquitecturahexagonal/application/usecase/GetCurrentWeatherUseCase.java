487808f21841888ad970ffcfe5a6783d0e4112ba
Julian Tovar <julian.tovar360@gmail.com>
2026-07-08 17:58:45 -0600

    feat: add application cape with use case for get current weather and manage exceptions

--- src/main/java/org/juliantovar/arquitecturahexagonal/application/usecase/GetCurrentWeatherUseCase.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/application/usecase/GetCurrentWeatherUseCase.java
@@ -1,5 +1,32 @@
 package org.juliantovar.arquitecturahexagonal.application.usecase;
 
-public interface GetCurrentWeatherUseCase {
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.juliantovar.arquitecturahexagonal.domain.exception.InvalidCoordinatesException;
+import org.juliantovar.arquitecturahexagonal.domain.model.GetCurrentWeatherCommand;
+import org.juliantovar.arquitecturahexagonal.domain.model.Weather;
+import org.juliantovar.arquitecturahexagonal.domain.port.WeatherClientPort;
+
+@ApplicationScoped
+public class GetCurrentWeatherUseCase {
+
+    private final WeatherClientPort weatherClientPort;
+
+    public GetCurrentWeatherUseCase(WeatherClientPort weatherClientPort) {
+        this.weatherClientPort = weatherClientPort;
+    }
+
+    public Uni<Weather> execute(GetCurrentWeatherCommand command) {
+
+        if (command.latitude() < -90 || command.latitude() > 90) {
+            throw new InvalidCoordinatesException("La latitud debe ser un valor entre -90 y 90");
+        }
+
+        if (command.longitude() < -180 || command.longitude() > 180) {
+            throw new InvalidCoordinatesException("La longitud debe ser un valor entre -180 y 180");
+        }
+
+        return weatherClientPort.getCurrentWeather(command.latitude(), command.longitude());
+    }
 
 }
