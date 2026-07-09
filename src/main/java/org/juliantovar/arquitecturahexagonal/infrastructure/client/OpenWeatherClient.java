d0ecc77e6f99f462276e9a28293a52d9fafc1b5c
Julian Tovar <julian.tovar360@gmail.com>
2026-07-09 11:24:49 -0600

    feat: add optional params to consume openweather api

--- src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/client/OpenWeatherClient.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/client/OpenWeatherClient.java
@@ -3,11 +3,14 @@ package org.juliantovar.arquitecturahexagonal.infrastructure.client;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.juliantovar.arquitecturahexagonal.infrastructure.dto.response.OpenWeatherResponseDto;
 
 @Path("/data/4.0/onecall")
+@Produces(MediaType.APPLICATION_JSON)
 @RegisterRestClient(configKey = "openweather-api")
 public interface OpenWeatherClient {
 
@@ -16,7 +19,9 @@ public interface OpenWeatherClient {
     Uni<OpenWeatherResponseDto> getCurrentWeather(
             @QueryParam("lat") Double latitude,
             @QueryParam("lon") Double longitude,
-            @QueryParam("appid") String apiKey
+            @QueryParam("appid") String apiKey,
+            @QueryParam("units") String units,
+            @QueryParam("lang") String language
     );
 
 }
