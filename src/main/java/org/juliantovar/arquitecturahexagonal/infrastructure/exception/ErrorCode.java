483c972135e11cd9ba0b03391feedd35e3ad0145
Julian Tovar <julian.tovar360@gmail.com>
2026-07-09 13:44:28 -0600

    feat: add error codes and error messages for api responses

---
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/exception/ErrorCode.java
@@ -0,0 +1,9 @@
+package org.juliantovar.arquitecturahexagonal.infrastructure.exception;
+
+public enum ErrorCode {
+    INVALID_COORDINATES,
+    WEATHER_NOT_FOUND,
+    WEATHER_SERVICE_UNAVAILABLE,
+    INVALID_API_KEY,
+    INTERNAL_SERVER_ERROR
+}
