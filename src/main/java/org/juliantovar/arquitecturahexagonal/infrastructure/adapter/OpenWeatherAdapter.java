43b586122c5db340f3e2f588ca74aeec23f88f43
Julian Tovar <julian.tovar360@gmail.com>
2026-07-09 11:25:39 -0600

    feat: add consumer adapter to consume openweather client with additional params

--- src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/adapter/OpenWeatherAdapter.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/adapter/OpenWeatherAdapter.java
@@ -1,13 +1,37 @@
 package org.juliantovar.arquitecturahexagonal.infrastructure.adapter;
 
 import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.juliantovar.arquitecturahexagonal.domain.model.Weather;
 import org.juliantovar.arquitecturahexagonal.domain.port.WeatherClientPort;
+import org.juliantovar.arquitecturahexagonal.infrastructure.client.OpenWeatherClient;
+import org.juliantovar.arquitecturahexagonal.infrastructure.config.OpenWeatherConfiguration;
+import org.juliantovar.arquitecturahexagonal.infrastructure.mapper.WeatherMapper;
 
+@ApplicationScoped
 public class OpenWeatherAdapter implements WeatherClientPort {
+
+    private final OpenWeatherClient client;
+    private final WeatherMapper mapper;
+    private final OpenWeatherConfiguration configuration;
+
+    public  OpenWeatherAdapter(@RestClient OpenWeatherClient client,
+                               WeatherMapper mapper,
+                               OpenWeatherConfiguration configuration) {
+        this.client = client;
+        this.mapper = mapper;
+        this.configuration = configuration;
+    }
 
     @Override
     public Uni<Weather> getCurrentWeather(Double latitude, Double longitude) {
-        return null;
+        return client.getCurrentWeather(
+                    latitude,
+                    longitude,
+                    configuration.apiKey(),
+                    configuration.client().units(),
+                    configuration.client().language()
+                ).map(mapper::toDomain);
     }
 }
