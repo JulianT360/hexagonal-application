30090d8a1105cd7cb2e1e223386c61ffecaa3c23
Julian Tovar <julian.tovar360@gmail.com>
2026-07-08 19:13:38 -0600

    feat: add open weather adapter

--- src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/adapter/OpenWeatherAdapter.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/adapter/OpenWeatherAdapter.java
@@ -1,4 +1,13 @@
 package org.juliantovar.arquitecturahexagonal.infrastructure.adapter;
 
-public interface OpenWeatherAdapter {
+import io.smallrye.mutiny.Uni;
+import org.juliantovar.arquitecturahexagonal.domain.model.Weather;
+import org.juliantovar.arquitecturahexagonal.domain.port.WeatherClientPort;
+
+public class OpenWeatherAdapter implements WeatherClientPort {
+
+    @Override
+    public Uni<Weather> getCurrentWeather(Double latitude, Double longitude) {
+        return null;
+    }
 }
