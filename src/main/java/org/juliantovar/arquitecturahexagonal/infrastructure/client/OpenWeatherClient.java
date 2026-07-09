eb50a853e20a8e026551751cfbfd85817321a4bb
Julian Tovar <julian.tovar360@gmail.com>
2026-07-08 19:25:17 -0600

    feat: add open weather rest client and config

--- src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/client/OpenWeatherClient.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/client/OpenWeatherClient.java
@@ -1,4 +1,22 @@
 package org.juliantovar.arquitecturahexagonal.infrastructure.client;
 
-public class OpenWeatherClient {
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.juliantovar.arquitecturahexagonal.infrastructure.dto.response.OpenWeatherResponseDto;
+
+@Path("/data/4.0/onecall")
+@RegisterRestClient(configKey = "openweather-api")
+public interface OpenWeatherClient {
+
+    @GET
+    @Path("/current")
+    Uni<OpenWeatherResponseDto> getCurrentWeather(
+            @QueryParam("lat") Double latitude,
+            @QueryParam("lon") Double longitude,
+            @QueryParam("appid") String apiKey
+    );
+
 }
