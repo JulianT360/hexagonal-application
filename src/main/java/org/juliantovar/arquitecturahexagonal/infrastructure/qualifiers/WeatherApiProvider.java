79d66f263cc55f11e7c7a950003e6965a4387ab7
Julian Tovar <julian.tovar360@gmail.com>
2026-07-10 12:43:42 -0600

    feat: adds qualifiers for every provider

---
+++ src/main/java/org/juliantovar/arquitecturahexagonal/infrastructure/qualifiers/WeatherApiProvider.java
@@ -0,0 +1,23 @@
+package org.juliantovar.arquitecturahexagonal.infrastructure.qualifiers;
+
+import jakarta.inject.Qualifier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Qualifier for Weather Api provider
+ *
+ * @author Julian Tovar
+ * @since 10/07/2026
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, FIELD, PARAMETER})
+public @interface WeatherApiProvider {
+}
