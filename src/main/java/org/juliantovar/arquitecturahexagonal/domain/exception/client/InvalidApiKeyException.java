059389be70df3a985d5639e77d4e6635865286ac
Julian Tovar <julian.tovar360@gmail.com>
2026-07-09 20:59:28 -0600

    feat: ajust configure exceptions by rest client

--- src/main/java/org/juliantovar/arquitecturahexagonal/domain/exception/client/InvalidApiKeyException.java
+++ src/main/java/org/juliantovar/arquitecturahexagonal/domain/exception/client/InvalidApiKeyException.java
@@ -1,4 +1,4 @@
-package org.juliantovar.arquitecturahexagonal.domain.exception;
+package org.juliantovar.arquitecturahexagonal.domain.exception.client;
 
 /**
  * Exception for invalid api key scenario.
@@ -6,7 +6,7 @@ package org.juliantovar.arquitecturahexagonal.domain.exception;
  * @author Julian Tovar
  * @since 09/07/2026
  */
-public class InvalidApiKeyException extends WeatherException {
+public class InvalidApiKeyException extends ClientException {
 
     public InvalidApiKeyException(String message) {
         super(message);
